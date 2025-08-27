2559808509e9bbb04afadc22b0f7a35270b5a2b6
Tafhim Khan <tafhimkhan39@gmail.com>
2025-08-27 03:30:39 -0400

    updated readme

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -14,13 +14,12 @@ SELECT
     salary_year_avg,
     job_posted_date,
     name AS company_name
-FROM
-    job_postings_fact
-LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+FROM job_postings_fact
+LEFT JOIN company_dim 
+    ON job_postings_fact.company_id = company_dim.company_id
 WHERE 
     job_title_short = 'Data Analyst' AND 
     job_location = 'Anywhere' AND
     salary_year_avg IS NOT NULL
-ORDER BY 
-    salary_year_avg DESC
+ORDER BY salary_year_avg DESC
 LIMIT 10
